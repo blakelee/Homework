abe2bf15589348df4db618a9554c38c52fb01c2b
Blake Oliveira <blake@blakelee.net>
2017-04-06 20:38:21 -0700

    Setup DialogFragment in view

--- app/src/main/kotlin/net/blakelee/homework/views/EditClassUI.kt
+++ app/src/main/kotlin/net/blakelee/homework/views/EditClassUI.kt
@@ -1,18 +1,17 @@
 package net.blakelee.homework.views
 
+import android.support.v7.app.AppCompatActivity
 import android.content.Intent
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ImageView
 import net.blakelee.homework.R
 import org.jetbrains.anko.*
 
 class EditClassUI : AnkoComponent<AppCompatActivity> {
-
     private val PICTURE_RESULT = 100
 
-    override fun createView(ui: AnkoContext<AppCompatActivity>): View = with(ui){
+    override fun createView(ui: AnkoContext<AppCompatActivity>): View = with(ui) {
         verticalLayout {
             imageView {
                 id = R.id.edit_class_image
@@ -29,14 +28,37 @@ class EditClassUI : AnkoComponent<AppCompatActivity> {
             }.lparams(width = matchParent, height = dip(200))
 
             verticalLayout {
-                val className = editText {
+                editText {
                     hint = ("Class Name")
+                    hintTextColor = ContextCompat.getColor(ctx, R.color.caldroid_light_red)
                 }
-                val professorName = editText {
-                    hint = "Professor Name"
-                }
-                padding = dip(14)
             }
+            button("Days") {
+                id = R.id.day_picker
+                setAllCaps(false)
+                /*val args = Bundle()
+                args.putIntegerArrayList("days", daysSelected)
+                with (DayPicker()) {
+                    arguments = args
+                    show(fragmentManager, "DAY_PICKER")
+                }*/
+            }
+            editText { hint = "Professor Name" }
+            editText { hint = "Location" }
+            editText { hint = "Email Address" }
+            editText { hint = "Phone Number" }
+            editText { hint = "Credit Hours" }
+            padding = dip(14)
         }
     }
 }
+
+/*fun AlertBuilder<DialogInterface>.multiItems(ctx: Context, items: List<CharSequence>, checked: BooleanArray, onItemSelected: (dialog: DialogInterface, index: Int, isChecked: Boolean) -> Unit) {
+    val builder = AlertDialog.Builder(ctx)
+    builder.setMultiChoiceItems(Array(items.size){ i -> items[i].toString() }, checked) {dialog, which, isChecked ->
+        onItemSelected(dialog, which, isChecked)
+    }
+    builder.create()
+    builder.show()
+}*/
+
