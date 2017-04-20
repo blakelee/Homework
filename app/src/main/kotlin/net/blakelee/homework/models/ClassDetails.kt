7f39af05c4f2c0859e97c9be47dd3bddbd1efdba
Blake Oliveira <blake@blakelee.net>
2017-04-20 00:48:50 -0700

    Added more items to ClassDetails

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
@@ -1,8 +1,13 @@
 package net.blakelee.homework.models
 
-data class ClassDetails(val name: String, val icon: String, val times: String,
-                        val location: String = "", val syllabus: String = "",
-                        val image: String = "", val professor: String = ""
-                        ) {
-
-}
+data class ClassDetails(val name: String,
+                        val days : MutableList<Days>,
+                        val location: String,
+                        val syllabus: String,
+                        val email: String,
+                        val phone: String,
+                        val hours: Int,
+                        val image: String,
+                        val professor: String,
+                        val finals: Days
+)
