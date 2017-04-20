088c7f1247880ba8120e63a124687b0752128e02
Blake Oliveira <blake@blakelee.net>
2017-04-20 00:46:51 -0700

    Add recycler to activity instead of view

--- app/src/main/kotlin/net/blakelee/homework/activities/EditClassActivity.kt
+++ app/src/main/kotlin/net/blakelee/homework/activities/EditClassActivity.kt
@@ -1,31 +1,41 @@
 package net.blakelee.homework.activities
 
 import android.app.Activity
+import android.app.ActivityManager
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.text.Editable
 import android.text.TextWatcher
 import android.text.format.DateUtils.LENGTH_SHORTEST
 import android.text.format.DateUtils.getDayOfWeekString
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import net.blakelee.homework.R
+import net.blakelee.homework.adapters.EditClassDayAdapter
 import net.blakelee.homework.fragments.DayPicker
+import net.blakelee.homework.interfaces.EditClassInterface
+import net.blakelee.homework.models.ClassDetails
+import net.blakelee.homework.models.Days
 import net.blakelee.homework.views.EditClassUI
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
-import java.util.*
+import kotlin.collections.ArrayList
 
-class EditClassActivity : AppCompatActivity(), DayPicker.DayDialogListener  {
+class EditClassActivity : AppCompatActivity(), EditClassInterface  {
 
     private val PICTURE_RESULT = 100
     private val imgView by lazy { find<ImageView>(R.id.edit_class_image) }
     private val dayButton by lazy  {find<Button>(R.id.day_picker )}
     private val phoneNumber by lazy {find<EditText>(R.id.phone_number)}
+    private val recycler by lazy {find<RecyclerView>(R.id.days_recycler)}
     private val text = StringBuilder()
     private var daysSelected = ArrayList<Int>()
+    var classDetails = ClassDetails("", mutableListOf(Days("None", "8:00am", "3:00pm")), "", "", "", "", 0, "", "", Days("Date", "8:00am", "3:00pm"))
 
     override fun onFinishEditDialog(daysSelected: ArrayList<Int>) {
         text.setLength(0)
@@ -41,20 +51,29 @@ class EditClassActivity : AppCompatActivity(), DayPicker.DayDialogListener  {
         this.daysSelected = daysSelected
     }
 
+    override fun openDaysDialog(daysSelected: ArrayList<Int>) {
+        val args = Bundle()
+        args.putIntegerArrayList("days", daysSelected)
+
+        val dp = DayPicker()
+        dp.arguments = args
+        dp.show(fragmentManager, "DAY_PICKER")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        EditClassUI().setContentView(this)
 
-        //Opens a dialogFragment to pick days of the week
-        dayButton.setOnClickListener {
-            val args = Bundle()
-            args.putIntegerArrayList("days", daysSelected)
+        /**
+         * This is temporary until I get a db added
+         */
+        //classDetails = ClassDetails("", mutableListOf(Days("None", "8:00am", "3:00pm")), "", "", "", "", 0, "", "", Days("Date", "8:00am", "3:00pm"))
 
-            val dp = DayPicker()
-            dp.arguments = args
-            dp.show(fragmentManager, "DAY_PICKER")
-        }
+        EditClassUI(classDetails, this).setContentView(this)
+
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = EditClassDayAdapter(classDetails.days, this, recycler)
+
 
         //Formats number to appear like (###) ###-####
         phoneNumber.addTextChangedListener(object : TextWatcher {
