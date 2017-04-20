6d063b604efd8f88cb5fddc2cee4e9e5d32e91b0
Blake Oliveira <blake@blakelee.net>
2017-04-20 02:48:39 -0700

    Checkpoint. This does not work. Trying to properly get viewholder maintained

--- app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
@@ -2,15 +2,19 @@ package net.blakelee.homework.models
 
 import android.app.Activity
 import android.app.FragmentManager
+import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.text.format.DateUtils
 import android.view.View
 import android.widget.Button
 import org.jetbrains.anko.find
 import net.blakelee.homework.R
 import net.blakelee.homework.adapters.EditClassDayAdapter
+import net.blakelee.homework.fragments.DayPicker
+import net.blakelee.homework.interfaces.EditClassDayInterface
 import net.blakelee.homework.interfaces.EditClassInterface
 
-class EditDaysItemViewHolder(item: View) : RecyclerView.ViewHolder(item), View.OnClickListener {
+class EditDaysItemViewHolder(item: View) : RecyclerView.ViewHolder(item), EditClassDayInterface {
     val day: Button = item.find(R.id.day_picker)
     val dayStart: Button = item.find(R.id.day_start)
     val dayEnd: Button = item.find(R.id.day_end)
@@ -22,10 +26,11 @@ class EditDaysItemViewHolder(item: View) : RecyclerView.ViewHolder(item), View.O
         day.text = days.day
         dayStart.text = days.startTime
         dayEnd.text = days.endTime
+        this.act = act
 
         val listener = act as EditClassInterface
         day.setOnClickListener {
-            listener.openDaysDialog(daysSelected)
+            listener.openDaysDialog(daysSelected, )
         }
 
         if (position > 0)
@@ -41,10 +46,18 @@ class EditDaysItemViewHolder(item: View) : RecyclerView.ViewHolder(item), View.O
         }
     }
 
-    override fun onClick(v: View?) {
-        val position = adapterPosition
-        if (position != RecyclerView.NO_POSITION) {
+    override fun dayPickerViewOnClick(daysSelected: ArrayList<Int>) {
+        val text = StringBuilder()
 
+        daysSelected.let {
+            for(item in it)
+                text.append(DateUtils.getDayOfWeekString(item + 1, DateUtils.LENGTH_SHORTEST)) //Add 1 because I did 0-6
         }
+
+        if (text.isEmpty())
+            text.append("None")
+
+        dayButton.text = text
+        this.daysSelected = daysSelected
     }
 }
