fb6f3fc424f4b4ec3d81e3d9af526af6f0abce4c
Blake Oliveira <blake@blakelee.net>
2017-04-20 00:49:03 -0700

    Changed readability

--- app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
@@ -1,3 +1,7 @@
 package net.blakelee.homework.models
 
-data class Classes (val name: String, val times: String, val icon: String, val id: Int)
+data class Classes (
+        val name: String,
+        val times: List<String>,
+        val icon: String
+)
