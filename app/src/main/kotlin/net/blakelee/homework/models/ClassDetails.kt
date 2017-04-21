03aad925d232d507af8875bb81465273ab72b292
Blake Oliveira <blake@blakelee.net>
2017-04-20 17:40:27 -0700

    Added default values to constructor

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
@@ -1,13 +1,13 @@
 package net.blakelee.homework.models
 
-data class ClassDetails(val name: String,
-                        val week: MutableList<Week>,
-                        val location: String,
-                        val syllabus: String,
-                        val email: String,
-                        val phone: String,
-                        val hours: Int?,
-                        val image: String,
-                        val professor: String,
-                        val finals: Day
+data class ClassDetails(val name: String = "",
+                        val week: MutableList<Week> = mutableListOf(Week()),
+                        val location: String = "",
+                        val syllabus: String = "",
+                        val email: String = "",
+                        val phone: String = "",
+                        val hours: Int? = null,
+                        val image: String = "",
+                        val professor: String = "",
+                        val finals: Day = Day()
 )
