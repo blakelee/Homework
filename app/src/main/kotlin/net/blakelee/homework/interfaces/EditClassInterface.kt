953038492fd9c23f32a9baea5fd1208cd33d91a6
Blake Oliveira <blake@blakelee.net>
2017-04-20 17:13:04 -0700

    Added view parameter

--- app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
@@ -3,6 +3,6 @@ package net.blakelee.homework.interfaces
 import android.view.View
 
 interface EditClassInterface {
-    fun openDaysDialog(daysSelected: ArrayList<Int>)
+    fun openDaysDialog(view: View, daysSelected: ArrayList<Int>)
     fun onFinishEditDialog(daysSelected : ArrayList<Int>)
 }
