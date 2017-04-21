02a8982756a8bc5422ecfc215d7add213f57f126
Blake Oliveira <blake@blakelee.net>
2017-04-20 17:11:45 -0700

    Significant refactor. Fixes bindings when values change as well as button listeners.

--- app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
@@ -1,63 +1,41 @@
 package net.blakelee.homework.models
 
-import android.app.Activity
-import android.app.FragmentManager
-import android.os.Bundle
 import android.support.v7.widget.RecyclerView
-import android.text.format.DateUtils
 import android.view.View
-import android.widget.Button
-import org.jetbrains.anko.find
-import net.blakelee.homework.R
-import net.blakelee.homework.adapters.EditClassDayAdapter
-import net.blakelee.homework.fragments.DayPicker
-import net.blakelee.homework.interfaces.EditClassDayInterface
+import kotlinx.android.synthetic.main.edit_class_days.view.*
+import net.blakelee.homework.interfaces.EditClassAdapterInterface
 import net.blakelee.homework.interfaces.EditClassInterface
 
-class EditDaysItemViewHolder(item: View) : RecyclerView.ViewHolder(item), EditClassDayInterface {
-    val day: Button = item.find(R.id.day_picker)
-    val dayStart: Button = item.find(R.id.day_start)
-    val dayEnd: Button = item.find(R.id.day_end)
-    var daysSelected = ArrayList<Int>()
-    val addremove: Button = item.find(R.id.addremove_day)
-    val item = item
+class EditDaysItemViewHolder(itemView: View, val editClassInterface: EditClassInterface, val editClassAdapterInterface: EditClassAdapterInterface) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(days : Days, act: Activity, position: Int, adapter: EditClassDayAdapter) {
-        day.text = days.day
-        dayStart.text = days.startTime
-        dayEnd.text = days.endTime
-        this.act = act
+    val daysSelected = ArrayList<Int>()
 
-        val listener = act as EditClassInterface
-        day.setOnClickListener {
-            listener.openDaysDialog(daysSelected, )
+    fun bind(week: Week, position: Int) {
+        with(itemView) {
+            day_picker.text = week.day.day
+            day_start.text = week.day.startTime
+            day_end.text = week.day.endTime
         }
 
-        if (position > 0)
-            addremove.text = "-"
+        daysSelected.clear()
 
-        addremove.setOnClickListener {
-            if (adapterPosition != RecyclerView.NO_POSITION) {
-                if (position > 0)
-                    adapter.remove(item)
-                else
-                    adapter.add(item)
+        //Add Remove Views
+        if (position > 0) {
+            itemView.addremove_day.text = "-"
+            itemView.addremove_day.setOnClickListener {
+                editClassAdapterInterface.removeView(itemView)
             }
         }
-    }
-
-    override fun dayPickerViewOnClick(daysSelected: ArrayList<Int>) {
-        val text = StringBuilder()
-
-        daysSelected.let {
-            for(item in it)
-                text.append(DateUtils.getDayOfWeekString(item + 1, DateUtils.LENGTH_SHORTEST)) //Add 1 because I did 0-6
+        else {
+            itemView.addremove_day.text = "+"
+            itemView.addremove_day.setOnClickListener {
+                editClassAdapterInterface.addView(itemView)
+            }
         }
 
-        if (text.isEmpty())
-            text.append("None")
-
-        dayButton.text = text
-        this.daysSelected = daysSelected
+        //Open Day Picker Dialog
+        itemView.day_picker.setOnClickListener {
+            editClassInterface.openDaysDialog(itemView, daysSelected)
+        }
     }
 }
