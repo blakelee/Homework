00913a193d5baa8ce75dc77c58a4f92d75ccc742
Blake Oliveira <blake@blakelee.net>
2017-04-20 17:12:14 -0700

    Small changes. Make hours nullable

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
@@ -1,13 +1,13 @@
 package net.blakelee.homework.models
 
 data class ClassDetails(val name: String,
-                        val days : MutableList<Days>,
+                        val week: MutableList<Week>,
                         val location: String,
                         val syllabus: String,
                         val email: String,
                         val phone: String,
-                        val hours: Int,
+                        val hours: Int?,
                         val image: String,
                         val professor: String,
-                        val finals: Days
+                        val finals: Day
 )
