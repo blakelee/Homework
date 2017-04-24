63b09d971b44ed61bb9bc837d0c58084a71fdf50
Blake Oliveira <blake@blakelee.net>
2017-04-23 19:22:18 -0700

    Changed "am" and "pm" to upper case to match the time picker

--- app/src/main/kotlin/net/blakelee/homework/models/Day.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Day.kt
@@ -2,6 +2,6 @@ package net.blakelee.homework.models
 
 data class Day (
     val day : String = "None",
-    val startTime : String = "8:00am",
-    val endTime : String = "3:00pm"
+    val startTime : String = "8:00AM",
+    val endTime : String = "10:00AM"
 )
