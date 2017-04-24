e76d1a76befe593dc5d699c073733e3208c40b45
Blake Oliveira <blake@blakelee.net>
2017-04-23 23:40:56 -0700

    Moved setting the onclick listener from the viewholder to the adapter removing the need for an interface

--- app/src/main/kotlin/net/blakelee/homework/adapters/EditClassDayAdapter.kt
+++ app/src/main/kotlin/net/blakelee/homework/adapters/EditClassDayAdapter.kt
@@ -3,26 +3,48 @@ package net.blakelee.homework.adapters
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.edit_class_days.view.*
+import net.blakelee.homework.R
 import net.blakelee.homework.models.EditDaysItemViewHolder
-import net.blakelee.homework.interfaces.EditClassAdapterInterface
 import net.blakelee.homework.interfaces.EditClassInterface
 import net.blakelee.homework.models.Week
 import net.blakelee.homework.views.WeekItemUI
 import org.jetbrains.anko.AnkoContext
 
-class EditClassDayAdapter(val week: MutableList<Week>, val editClassInterface: EditClassInterface, val recycler: RecyclerView) : RecyclerView.Adapter<EditDaysItemViewHolder>(), EditClassAdapterInterface {
+class EditClassDayAdapter(val week: MutableList<Week>, val editClassInterface: EditClassInterface, val recycler: RecyclerView) : RecyclerView.Adapter<EditDaysItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EditDaysItemViewHolder =
-            EditDaysItemViewHolder(WeekItemUI().createView(AnkoContext.create(parent.context, parent)), editClassInterface, this)
+            EditDaysItemViewHolder(WeekItemUI().createView(AnkoContext.create(parent.context, parent)))
 
     override fun onBindViewHolder(holder: EditDaysItemViewHolder?, position: Int) {
-        val day = week[position]
-        holder!!.bind(day, position)
+        val week = week[position]
+        holder!!.bind(week, position)
+        holder.itemView.day_start
+
+        //Add Remove Views
+        if (position > 0)
+            holder.itemView.addremove_day.setOnClickListener { removeView(holder.itemView) }
+        else
+            holder.itemView.addremove_day.setOnClickListener { addView(holder.itemView) }
+
+        //Open Day Picker Dialog
+        holder.itemView.day_picker.setOnClickListener { editClassInterface.openDaysDialog(holder.itemView, holder.daysSelected) }
+
+        //Open Time Picker
+        holder.itemView.day_start.setOnClickListener {
+            holder.itemView.id = R.id.day_start
+            editClassInterface.openTimePicker(holder.itemView, week.day.startTime)
+        }
+
+        holder.itemView.day_end.setOnClickListener {
+            holder.itemView.id = R.id.day_end
+            editClassInterface.openTimePicker(holder.itemView, week.day.endTime)
+        }
     }
 
     override fun getItemCount(): Int = week.size
 
-    override fun removeView(view: View) {
+    fun removeView(view: View) {
         val position = recycler.getChildAdapterPosition(view)
         if (position != RecyclerView.NO_POSITION) {
             week.removeAt(position)
@@ -30,7 +52,7 @@ class EditClassDayAdapter(val week: MutableList<Week>, val editClassInterface: E
         }
     }
 
-    override fun addView(view: View) {
+    fun addView(view: View) {
         if (itemCount < 5) {
             val position = recycler.getChildAdapterPosition(view)
             if (position != RecyclerView.NO_POSITION) {
--- app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
@@ -3,11 +3,8 @@ package net.blakelee.homework.models
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.synthetic.main.edit_class_days.view.*
-import net.blakelee.homework.R
-import net.blakelee.homework.interfaces.EditClassAdapterInterface
-import net.blakelee.homework.interfaces.EditClassInterface
 
-class EditDaysItemViewHolder(itemView: View, val editClassInterface: EditClassInterface, val editClassAdapterInterface: EditClassAdapterInterface) : RecyclerView.ViewHolder(itemView) {
+class EditDaysItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val daysSelected = ArrayList<Int>()
 
@@ -20,34 +17,9 @@ class EditDaysItemViewHolder(itemView: View, val editClassInterface: EditClassIn
 
         daysSelected.clear()
 
-        //Add Remove Views
-        if (position > 0) {
+        if (position > 0)
             itemView.addremove_day.text = "-"
-            itemView.addremove_day.setOnClickListener {
-                editClassAdapterInterface.removeView(itemView)
-            }
-        }
-        else {
+        else
             itemView.addremove_day.text = "+"
-            itemView.addremove_day.setOnClickListener {
-                editClassAdapterInterface.addView(itemView)
-            }
-        }
-
-        //Open Day Picker Dialog
-        itemView.day_picker.setOnClickListener {
-            editClassInterface.openDaysDialog(itemView, daysSelected)
-        }
-
-        //Open Time Picker
-        itemView.day_start.setOnClickListener {
-            itemView.id = R.id.day_start
-            editClassInterface.openTimePicker(itemView, week.day.startTime)
-        }
-
-        itemView.day_end.setOnClickListener {
-            itemView.id = R.id.day_end
-            editClassInterface.openTimePicker(itemView, week.day.endTime)
-        }
     }
 }
