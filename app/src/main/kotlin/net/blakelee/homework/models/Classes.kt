b36975e7fe37922b922fe43578a0210f0d94e872
Blake Oliveira <blake@blakelee.net>
2017-04-25 01:22:27 -0700

    Set default values for dummy data

--- app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
@@ -1,7 +1,8 @@
 package net.blakelee.homework.models
 
+//TODO: Change to null since this is just dummy data
 data class Classes (
-        val name: String,
-        val times: List<String>,
-        val icon: String
+        val name: String = "Psychology 101",
+        val times: List<String> = listOf("MWF 8:00am-10:00am", "TT 10:00am-11:00am", "S 12:00pm-1:00pm"),
+        val icon: String = ""
 )
