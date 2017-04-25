f4366a53281e8819ea93211cf3a2fc6e529f35aa
Blake Oliveira <blake@blakelee.net>
2017-04-25 01:32:02 -0700

    initial commit

---
+++ app/src/main/kotlin/net/blakelee/homework/activities/ClassDetailsActivity.kt
@@ -0,0 +1,43 @@
+package net.blakelee.homework.activities
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.FragmentActivity
+import android.support.v4.view.ViewPager
+import net.blakelee.homework.R
+import net.blakelee.homework.adapters.ClassDetailsPagerAdapter
+import net.blakelee.homework.models.ClassDetails
+import net.blakelee.homework.models.Day
+import net.blakelee.homework.models.Week
+import net.blakelee.homework.views.ClassDetailsUI
+import org.jetbrains.anko.find
+import org.jetbrains.anko.setContentView
+
+class ClassDetailsActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Dummy data
+        val classDetails = ClassDetails("Psychology 101", mutableListOf(Week(), Week()), "", "", "test@gmail.com", "(555) 123-4567", null, "", "Mrs. Puff", Day())
+
+        ClassDetailsUI().setContentView(this)
+
+        val tabLayout = find<TabLayout>(R.id.tab_layout)
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val pager = find<ViewPager>(R.id.class_pager)
+        val pagerAdapter = ClassDetailsPagerAdapter(supportFragmentManager, 1, classDetails)
+
+        pager.adapter = pagerAdapter
+        pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                pager.currentItem = tab!!.position
+            }
+        })
+    }
+}
