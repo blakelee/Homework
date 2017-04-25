7ff2dc1de79c3eb97619e4e29a0f924a5a0d64ae
Blake Oliveira <blake@blakelee.net>
2017-04-25 01:21:50 -0700

    Made clicking on item start activity

--- app/src/main/kotlin/net/blakelee/homework/models/MainItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/MainItemViewHolder.kt
@@ -1,27 +1,32 @@
 package net.blakelee.homework.models
 
-import android.graphics.drawable.Drawable
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import org.jetbrains.anko.find
 import net.blakelee.homework.R
+import net.blakelee.homework.activities.ClassDetailsActivity
+import org.jetbrains.anko.find
 
-
-class MainItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
-    var icon: ImageView = view.find(R.id.class_icon)
-    val name: TextView = view.find(R.id.class_name)
-    val times: TextView = view.find(R.id.class_times)
+class MainItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+   var icon: ImageView = view.find(R.id.class_icon)
+   val name: TextView = view.find(R.id.class_name)
+   val times: TextView = view.find(R.id.class_times)
 
     fun init(classes: Classes) {
-        icon.setImageDrawable(Drawable.createFromPath(classes.toString()))
+        icon.setImageResource(R.drawable.science)
         name.text = classes.name
-
         var s : String = ""
-        for(s2: String in classes.times) {
+        for(s2: String in classes.times)
             s = s + s2 + "\n"
-        }
 
+        s = s.dropLast(1)
+        times.text = s
+
+        itemView.setOnClickListener {
+            //TODO: set bundle to name of class instead of passing dummy data
+            itemView?.context?.startActivity(Intent(itemView.context, ClassDetailsActivity().javaClass))
+        }
     }
 }
