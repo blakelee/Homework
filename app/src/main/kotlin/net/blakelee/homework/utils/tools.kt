a8f067b1bc2eca84b16a9e180eb3dae754b2a809
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:24:56 -0700

    Moved AlertBuilder to here since it wasn't being used.

--- app/src/main/kotlin/net/blakelee/homework/utils/tools.kt
+++ app/src/main/kotlin/net/blakelee/homework/utils/tools.kt
@@ -1,10 +1,23 @@
 package net.blakelee.homework.utils
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.AlertBuilder
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
+
+fun AlertBuilder<DialogInterface>.multiItems(ctx: Context, items: List<CharSequence>, checked: BooleanArray, onItemSelected: (dialog: DialogInterface, index: Int, isChecked: Boolean) -> Unit) {
+    val builder = AlertDialog.Builder(ctx)
+    builder.setMultiChoiceItems(Array(items.size){ i -> items[i].toString() }, checked) {dialog, which, isChecked ->
+        onItemSelected(dialog, which, isChecked)
+    }
+    builder.create()
+    builder.show()
 }
