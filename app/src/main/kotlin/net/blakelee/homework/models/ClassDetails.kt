be6746019f2e7f2b1c66943bb3f5f733f356893f
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:08:47 -0700

    Changes for Realm usage

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
@@ -1,13 +1,25 @@
 package net.blakelee.homework.models
 
-data class ClassDetails(var name: String = "",
-                        var week: MutableList<Week> = mutableListOf(Week()),
-                        var location: String = "",
-                        var syllabus: String = "",
-                        var email: String = "",
-                        var phone: String = "",
-                        var hours: Int? = null,
-                        var image: String = "",
-                        var professor: String = "",
-                        var finals: Day = Day()
-)
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.annotations.Index
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+@RealmClass
+open class ClassDetails (
+
+        @PrimaryKey
+        @Index
+        open var name: String = "",
+
+        open var week: RealmList<Week> = RealmList(Week()),
+        open var location: String = "",
+        open var syllabus: String = "",
+        open var email: String = "",
+        open var phone: String = "",
+        open var hours: Int? = null,
+        open var image: String = "",
+        open var professor: String = "",
+        open var finals: Day = Day()
+) : RealmObject()
