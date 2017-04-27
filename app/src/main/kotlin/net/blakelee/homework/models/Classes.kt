7de4db4dae7e2a11b9c51e7c8c6f7ebb2d3d126f
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:12:11 -0700

    Changed class for Realm

--- app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
@@ -1,8 +1,25 @@
 package net.blakelee.homework.models
 
+import io.realm.RealmObject
+import io.realm.annotations.Index
+import io.realm.annotations.PrimaryKey
+
 //TODO: Change to null since this is just dummy data
-data class Classes (
-        val name: String = "Psychology 101",
-        val times: List<String> = listOf("MWF 8:00am-10:00am", "TT 10:00am-11:00am", "S 12:00pm-1:00pm"),
-        val icon: String = ""
-)
+open class Classes (
+
+        @PrimaryKey
+        @Index
+        var name: String = "Psychology 101",
+
+        var times: String = "MWF 8:00am-10:00am,,,,TT 10:00am-11:00am,,,,S 12:00pm-1:00pm",
+        var icon: String = ""
+) : RealmObject() {
+
+        fun getTimes() : List<String> = times.split(",,,,")
+        fun setTimes(weeks : List<String>?) {
+                if (weeks == null)
+                        times = ""
+                else
+                        times = weeks.joinToString(",,,,")
+        }
+}
