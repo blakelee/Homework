02bfe65c990d3f198e2215b8927576bb9084cc23
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:04:13 -0700

    Small changes for refactor

--- app/src/main/kotlin/net/blakelee/homework/fragments/DayPicker.kt
+++ app/src/main/kotlin/net/blakelee/homework/fragments/DayPicker.kt
@@ -9,27 +9,30 @@ import android.text.format.DateUtils.getDayOfWeekString
 import net.blakelee.homework.interfaces.EditClassInterface
 import java.util.*
 
-class DayPicker(val daysSelected : ArrayList<Int>, val position : Int) : DialogFragment() {
+class DayPicker(val oldDaysSelected : List<String>, val position : Int) : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
         val ba = BooleanArray(7)
+        val daysSelected : ArrayList<String> = ArrayList(oldDaysSelected)
 
-        for(i in daysSelected)
-            ba[i] = true
+        oldDaysSelected.map {
+            ba[it.toInt()] = true
+        }
+
 
         with(builder) {
             setTitle("Class Days")
             setMultiChoiceItems(Array(7) { i -> getDayOfWeekString(i + 1, LENGTH_LONG) }, ba) { _, which, isChecked ->
                 if (isChecked)
-                    daysSelected.add(which)
-                else if (daysSelected.contains(which))
-                    daysSelected.remove(which)
+                    daysSelected.add(which.toString())
+                else
+                    daysSelected.remove(which.toString())
             }
             setPositiveButton("OK") { _, _ ->
                 daysSelected.sort()
                 val listener = activity as EditClassInterface
-                listener.onFinishEditDialog(daysSelected, position)
+                listener.onFinishEditDialog(daysSelected.toList(), position)
             }
         }
         return builder.create()
