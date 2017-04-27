e1580ae523f2bb52e1cfb331fdbc16a5e8bf4124
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:08:09 -0700

    Changed types to better fit with MVP and Realm

--- app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
@@ -1,7 +1,11 @@
 package net.blakelee.homework.interfaces
 
+import net.blakelee.homework.models.ClassDetails
+import java.util.*
+
 interface EditClassInterface {
-    fun openDaysDialog(daysSelected: ArrayList<Int>, position: Int)
-    fun onFinishEditDialog(daysSelected : ArrayList<Int>, position : Int)
-    fun openTimePicker(time: String, compareTime: (String) -> Int)
+    fun openDaysDialog(daysSelected: List<String>, position: Int)
+    fun onFinishEditDialog(daysSelected : List<String>, position : Int)
+    fun openTimePicker(date : Date, compareTime: (Date) -> Int)
+    fun setClassDetails(classDetails : ClassDetails)
 }
