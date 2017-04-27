63d57bbd34d9d4eaf90efa95aa98705426276330
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:23:25 -0700

    Take out kotlinx since it was referencing the xml

--- app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/EditDaysItemViewHolder.kt
@@ -2,24 +2,25 @@ package net.blakelee.homework.models
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import kotlinx.android.synthetic.main.edit_class_days.view.*
+import android.widget.Button
+import net.blakelee.homework.R
+import org.jetbrains.anko.find
 
-class EditDaysItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class EditDaysItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    val daysSelected = ArrayList<Int>()
+    val day_picker = view.find<Button>(R.id.day_picker)
+    val day_start = view.find<Button>(R.id.day_start)
+    val day_end = view.find<Button>(R.id.day_end)
+    val add_remove = view.find<Button>(R.id.addremove_day)
 
     fun bind(week: Week, position: Int) {
-        with(itemView) {
-            day_picker.text = week.day.day
-            day_start.text = week.day.startTime
-            day_end.text = week.day.endTime
-        }
-
-        daysSelected.clear()
+        day_picker.text = week.day.day
+        day_start.text = week.day.getStartTime()
+        day_end.text = week.day.getEndTime()
 
         if (position > 0)
-            itemView.addremove_day.text = "-"
+            add_remove.text = "-"
         else
-            itemView.addremove_day.text = "+"
+            add_remove.text = "+"
     }
 }
