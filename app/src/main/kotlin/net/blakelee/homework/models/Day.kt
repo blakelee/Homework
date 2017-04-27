6392794a7514765418e070d31b16f454b89d2c4f
Blake Oliveira <blake@blakelee.net>
2017-04-27 01:13:11 -0700

    Changes for realm

--- app/src/main/kotlin/net/blakelee/homework/models/Day.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Day.kt
@@ -1,7 +1,36 @@
 package net.blakelee.homework.models
 
-data class Day (
+import io.realm.RealmObject
+import io.realm.annotations.Ignore
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+open class Day (
     var day : String = "None",
-    var startTime : String = "8:00AM",
-    var endTime : String = "10:00AM"
-)
+    var startTime : Date = Date(),
+    var endTime : Date = Date()
+
+) : RealmObject() {
+
+    @Ignore
+    var dateFormat : DateFormat = SimpleDateFormat("h:mma", Locale.getDefault())
+
+    //This sets default startTime to "8:00AM" and endTime to "10:00AM"
+    init {
+        startTime = dateFormat.parse("8:00AM")
+        endTime = dateFormat.parse("10:00AM")
+    }
+
+
+    fun getStartTime() : String = dateFormat.format(startTime)
+    fun setStartTime(time : String) {
+        startTime = dateFormat.parse(time)
+    }
+
+    fun getEndTime() : String = dateFormat.format(endTime)
+    fun setEndTime(time : String) {
+        endTime = dateFormat.parse(time)
+    }
+
+}
--- app/src/main/kotlin/net/blakelee/homework/models/Week.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Week.kt
@@ -1,6 +1,24 @@
 package net.blakelee.homework.models
 
-data class Week (
-        var week : ArrayList<Int> = ArrayList(7),
-        var day : Day = Day()
-)
+import io.realm.RealmObject
+
+open class Week (
+    open var week: String = "",
+    open var day : Day = Day()
+
+) : RealmObject() {
+
+    fun getWeek() : List<String> {
+        val week = week.split(",,,,")
+        if (week[0] == "")
+            return listOf()
+        return week
+    }
+
+    fun setWeek(weeks : List<String>?) {
+        if (weeks == null)
+            week = ""
+        else
+            week = weeks.joinToString(",,,,")
+    }
+}
