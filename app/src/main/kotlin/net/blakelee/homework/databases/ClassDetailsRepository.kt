eef890a87af9adc215905fab2bdcd0e33bae184a
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:40:48 -0700

    Got working with dbflow

--- app/src/main/kotlin/net/blakelee/homework/databases/ClassDetailsRepository.kt
+++ app/src/main/kotlin/net/blakelee/homework/databases/ClassDetailsRepository.kt
@@ -1,31 +1,59 @@
 package net.blakelee.homework.databases
 
-import io.realm.Realm
+import com.raizlabs.android.dbflow.kotlinextensions.from
+import com.raizlabs.android.dbflow.kotlinextensions.save
+import com.raizlabs.android.dbflow.kotlinextensions.update
+import com.raizlabs.android.dbflow.sql.language.SQLite
 import net.blakelee.homework.interfaces.ClassDetailsRepositoryInterface
 import net.blakelee.homework.models.ClassDetails
+import net.blakelee.homework.models.ClassDetailsJSON
+import net.blakelee.homework.models.ClassDetailsJSON_Table
+import net.blakelee.homework.models.Week
+import net.blakelee.homework.presenters.EditClassPresenter
 
-class ClassDetailsRepository : ClassDetailsRepositoryInterface {
+class ClassDetailsRepository(val editClassPresenter: EditClassPresenter) : ClassDetailsRepositoryInterface {
 
-    override fun addClass(classDetails: ClassDetails, callback: () -> Unit) {
-        val realm = Realm.getDefaultInstance()
-        realm.beginTransaction()
-        realm.createObject(classDetails.javaClass)
-        realm.commitTransaction()
-        callback()
+    val json = ClassDetailsJSON()
+
+    //Need to make sure adding a class doesn't conflict with an already existing class
+    override fun addClass(classDetails: ClassDetails) {
+        json.setClassDetailsFromClass(classDetails)
+        json.name = classDetails.name
+
+        val cd = SQLite.select()
+                .from(ClassDetailsJSON::class)
+                .where(ClassDetailsJSON_Table.name.eq(classDetails.name))
+                .querySingle()
+
+
+        if (cd == null) { //Success
+            json.save()
+            editClassPresenter.onAddClassSuccess()
+        }
+        else
+            editClassPresenter.onAddClassFailure()
     }
 
-    override fun changeClass(classDetails: ClassDetails, callback: () -> Unit) {
-        val realm = Realm.getDefaultInstance()
-        realm.beginTransaction()
-        realm.copyToRealmOrUpdate(classDetails)
-        realm.commitTransaction()
-        callback()
+    override fun changeClass(classDetails: ClassDetails) {
+        json.setClassDetailsFromClass(classDetails)
+        json.name = classDetails.name
+        json.update()
+        editClassPresenter.onChangeClassSuccess()
     }
 
-    override fun getClass(className: String, callback: (ClassDetails) -> Unit) {
-        val realm = Realm.getDefaultInstance()
-        val query = realm.where(ClassDetails::class.java)
-        val results = query.findFirst()
-        callback(results)
+    override fun getClass(className: String) {
+        val cd = SQLite.select()
+                .from(ClassDetailsJSON::class)
+                .where(ClassDetailsJSON_Table.name.eq(className))
+                .querySingle()
+
+        if (cd == null)
+            editClassPresenter.onGetClassFailure(ClassDetails())
+        else
+            editClassPresenter.onGetClassSuccess(cd.getClassDetailsFromString()!!)
+    }
+
+    override fun getWeek(className: String) : MutableList<Week> {
+        return mutableListOf(Week())
     }
 }
