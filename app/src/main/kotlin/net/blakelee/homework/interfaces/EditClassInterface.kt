6ed77bca96ff7ba2ee7b44e97d27ba041a324cb6
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:43:11 -0700

    Added validate

--- app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassInterface.kt
@@ -4,8 +4,9 @@ import net.blakelee.homework.models.ClassDetails
 import java.util.*
 
 interface EditClassInterface {
-    fun openDaysDialog(daysSelected: List<String>, position: Int)
-    fun onFinishEditDialog(daysSelected : List<String>, position : Int)
+    fun openDaysDialog(daysSelected: List<Int>, position: Int)
+    fun onFinishEditDialog(daysSelected : List<Int>, position : Int)
     fun openTimePicker(date : Date, compareTime: (Date) -> Int)
     fun setClassDetails(classDetails : ClassDetails)
+    fun validate(validName : Boolean) : Boolean
 }
