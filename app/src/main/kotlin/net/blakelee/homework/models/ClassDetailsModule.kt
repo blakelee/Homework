d46563ff496ae2fccf34542e45b0ecb50db83ca1
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:40:19 -0700

    Switched to dbflow

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetailsModule.kt
+++
@@ -1,6 +0,0 @@
-package net.blakelee.homework.models
-
-import io.realm.annotations.RealmModule
-
-@RealmModule(classes = arrayOf(ClassDetails::class, Week::class, Day::class))
-class ClassDetailsModule
