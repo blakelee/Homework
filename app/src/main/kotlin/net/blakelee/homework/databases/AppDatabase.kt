b30ebf3693053a41c949c748c9d8eed4c3ef6f3d
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:50:51 -0700

    dbflow config

---
+++ app/src/main/kotlin/net/blakelee/homework/databases/AppDatabase.kt
@@ -0,0 +1,10 @@
+package net.blakelee.homework.databases
+
+import com.raizlabs.android.dbflow.annotation.Database
+
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION, generatedClassSeparator = "_")
+object AppDatabase {
+    const val NAME: String = "app"
+    const val VERSION: Int = 1
+
+}
