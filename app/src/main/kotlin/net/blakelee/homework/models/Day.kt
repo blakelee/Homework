9d6ae9df7636fe093d01c2a2ec2c54e701bbe899
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:42:05 -0700

    Switched to inheritance of base day now

--- app/src/main/kotlin/net/blakelee/homework/models/Day.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Day.kt
@@ -1,36 +1,21 @@
 package net.blakelee.homework.models
 
-import io.realm.RealmObject
-import io.realm.annotations.Ignore
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import com.raizlabs.android.dbflow.annotation.Column
+import com.raizlabs.android.dbflow.annotation.Table
+import net.blakelee.homework.databases.AppDatabase
 import java.util.*
 
+@Table(database = AppDatabase::class)
 open class Day (
-    var day : String = "None",
-    var startTime : Date = Date(),
-    var endTime : Date = Date()
+        @Column
+       var day : Date? = null
+) : BaseDay() {
 
-) : RealmObject() {
+    fun  getDay(): String {
+        day?.let {
+            return day.toString()
+        }
 
-    @Ignore
-    var dateFormat : DateFormat = SimpleDateFormat("h:mma", Locale.getDefault())
-
-    //This sets default startTime to "8:00AM" and endTime to "10:00AM"
-    init {
-        startTime = dateFormat.parse("8:00AM")
-        endTime = dateFormat.parse("10:00AM")
+        return "None"
     }
-
-
-    fun getStartTime() : String = dateFormat.format(startTime)
-    fun setStartTime(time : String) {
-        startTime = dateFormat.parse(time)
-    }
-
-    fun getEndTime() : String = dateFormat.format(endTime)
-    fun setEndTime(time : String) {
-        endTime = dateFormat.parse(time)
-    }
-
 }
