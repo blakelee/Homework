31286cc4822aeac822462dad28cbf91ae6c060a2
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:43:44 -0700

    added ongetclassfailure and onaddclassfailure

--- app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassPresenterInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassPresenterInterface.kt
@@ -5,7 +5,9 @@ import net.blakelee.homework.models.ClassDetails
 
 interface EditClassPresenterInterface {
     fun onGetClassSuccess(classDetails : ClassDetails)
+    fun onGetClassFailure(classDetails : ClassDetails)
     fun onChangeClassSuccess()
     fun onAddClassSuccess()
+    fun onAddClassFailure()
     fun onDestroy()
 }
