a3f008be5d4e42d5a2a1c6b336d6a462923b62f7
Blake Oliveira <blake@blakelee.net>
2017-05-10 14:41:30 -0700

    switched to dbflow so I don't need strange conversions

--- app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Classes.kt
@@ -1,25 +1,16 @@
 package net.blakelee.homework.models
 
-import io.realm.RealmObject
-import io.realm.annotations.Index
-import io.realm.annotations.PrimaryKey
+import com.raizlabs.android.dbflow.annotation.PrimaryKey
+import com.raizlabs.android.dbflow.annotation.Table
+import net.blakelee.homework.databases.AppDatabase
 
 //TODO: Change to null since this is just dummy data
+@Table(database = AppDatabase::class)
 open class Classes (
 
         @PrimaryKey
-        @Index
         var name: String = "Psychology 101",
 
-        var times: String = "MWF 8:00am-10:00am,,,,TT 10:00am-11:00am,,,,S 12:00pm-1:00pm",
+        var times: List<String> = listOf("MWF 8:00am-10:00am", "TT 10:00am-11:00am", "S 12:00pm-1:00pm"),
         var icon: String = ""
-) : RealmObject() {
-
-        fun getTimes() : List<String> = times.split(",,,,")
-        fun setTimes(weeks : List<String>?) {
-                if (weeks == null)
-                        times = ""
-                else
-                        times = weeks.joinToString(",,,,")
-        }
-}
+)
