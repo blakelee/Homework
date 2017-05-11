213b5efde05f9ef2c68e33f3a14dc08ff9981fcc
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:29:11 -0700

    standardized names

--- app/src/main/kotlin/net/blakelee/homework/interfaces/ClassDetailsRepositoryInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/ClassDetailsRepositoryInterface.kt
@@ -1,11 +1,10 @@
 package net.blakelee.homework.interfaces
 
 import net.blakelee.homework.models.ClassDetails
-import net.blakelee.homework.models.Week
 
 interface ClassDetailsRepositoryInterface {
-    fun addClass(classDetails: ClassDetails)
-    fun getClass(className : String)
-    fun changeClass(classDetails : ClassDetails)
-    fun getWeek(className: String) : MutableList<Week>
+    fun addClass(classDetails: ClassDetails) : Boolean
+    fun getClass(classId : Int?) : ClassDetails
+    fun changeClass(classDetails : ClassDetails) : Boolean
+    fun deleteClass(classId : Int) : Boolean
 }
