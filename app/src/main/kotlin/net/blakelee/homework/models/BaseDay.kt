78e868f1382bb714ed1b383131665c5bc945c78c
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:25:05 -0700

    Unnecessary code

--- app/src/main/kotlin/net/blakelee/homework/models/BaseDay.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/BaseDay.kt
@@ -4,20 +4,13 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-open class BaseDay (
-    var startTime : Date = Date(),
-    var endTime : Date = Date()
-) {
+open class BaseDay {
 
     @Transient
     var dateFormat : DateFormat = SimpleDateFormat("h:mma", Locale.getDefault())
 
-    //This sets default startTime to "8:00AM" and endTime to "10:00AM"
-    init {
-        startTime = dateFormat.parse("8:00AM")
-        endTime = dateFormat.parse("10:00AM")
-    }
-
+    var startTime : Date = dateFormat.parse("8:00AM")
+    var endTime : Date = dateFormat.parse("10:00AM")
 
     fun getStartTime() : String = dateFormat.format(startTime)
     fun setStartTime(time : String) {
