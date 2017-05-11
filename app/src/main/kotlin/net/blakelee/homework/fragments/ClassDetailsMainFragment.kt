819959407afa49aa3b5c79d76183bc4c0243b90d
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:27:10 -0700

    Added fragment

--- app/src/main/kotlin/net/blakelee/homework/fragments/ClassDetailsMainFragment.kt
+++ app/src/main/kotlin/net/blakelee/homework/fragments/ClassDetailsMainFragment.kt
@@ -5,14 +5,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import net.blakelee.homework.databases.ClassDetailsRepository
 import net.blakelee.homework.models.ClassDetails
+import net.blakelee.homework.presenters.ClassDetailsPresenter
 import net.blakelee.homework.views.ClassDetailsMainFragmentUI
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.support.v4.ctx
 
-class ClassDetailsMainFragment(val classDetails: ClassDetails) : Fragment() {
+class ClassDetailsMainFragment(val classId: Int) : Fragment() {
+
+    lateinit var classDetails : ClassDetails
+    lateinit var presenter : ClassDetailsPresenter
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        presenter = ClassDetailsPresenter(classId, this)
+        classDetails = presenter.loadClassDetails(classId)
         return ClassDetailsMainFragmentUI(classDetails).createView(AnkoContext.create(ctx, this))
     }
 }
