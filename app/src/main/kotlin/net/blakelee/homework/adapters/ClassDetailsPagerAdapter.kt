ae1b03ec20cf2bb98febeab49374bf23cb4f12fa
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:27:42 -0700

    Loads details by id now

--- app/src/main/kotlin/net/blakelee/homework/adapters/ClassDetailsPagerAdapter.kt
+++ app/src/main/kotlin/net/blakelee/homework/adapters/ClassDetailsPagerAdapter.kt
@@ -6,13 +6,13 @@ import android.support.v4.app.FragmentStatePagerAdapter
 import net.blakelee.homework.fragments.ClassDetailsMainFragment
 import net.blakelee.homework.models.ClassDetails
 
-class ClassDetailsPagerAdapter(fragmentManager: FragmentManager, var numTabs: Int, val classDetails: ClassDetails) : FragmentStatePagerAdapter(fragmentManager){
+class ClassDetailsPagerAdapter(fragmentManager: FragmentManager, var numTabs: Int, val classId: Int) : FragmentStatePagerAdapter(fragmentManager){
 
     override fun getCount(): Int = numTabs
 
     override fun getItem(position: Int): Fragment? {
         when (position) {
-            0 -> return ClassDetailsMainFragment(classDetails)
+            0 -> return ClassDetailsMainFragment(classId)
             else -> return null
         }
     }
