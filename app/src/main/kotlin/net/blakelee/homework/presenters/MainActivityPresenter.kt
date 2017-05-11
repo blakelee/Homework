17049b45b737fe6446ea26ce29fea382e3d227ec
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:37:43 -0700

    Main activity presenter stub

--- app/src/main/kotlin/net/blakelee/homework/presenters/MainActivityPresenter.kt
+++ app/src/main/kotlin/net/blakelee/homework/presenters/MainActivityPresenter.kt
@@ -1,10 +1,19 @@
 package net.blakelee.homework.presenters
 
+import net.blakelee.homework.activities.MainActivity
+import net.blakelee.homework.databases.ClassesRepository
+import net.blakelee.homework.interfaces.MainActivityPresenterInterface
+import net.blakelee.homework.models.Classes
 
-class MainActivityPresenter {
+class MainActivityPresenter (private var view : MainActivity?) : MainActivityPresenterInterface{
 
-    fun getSubject(index: Int) {
+    private val classesRepository = ClassesRepository()
 
+    override fun loadClasses() : MutableList<Classes> {
+        return classesRepository.getClasses()
+    }
+    override fun onDestroy() {
+        view = null
     }
 
 }
