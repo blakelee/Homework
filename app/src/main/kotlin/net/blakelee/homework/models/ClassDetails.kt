f2a346001bccdc7814fac1badd4becf91467ab44
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:26:04 -0700

    Fixed weeks list problem

--- app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/ClassDetails.kt
@@ -2,14 +2,16 @@ package net.blakelee.homework.models
 
 import com.raizlabs.android.dbflow.annotation.PrimaryKey
 import com.raizlabs.android.dbflow.annotation.Table
+import com.raizlabs.android.dbflow.structure.BaseModel
 import net.blakelee.homework.databases.AppDatabase
 
-@Table(database = AppDatabase::class)
+@Table(database = AppDatabase::class, allFields = true)
 class ClassDetails (
 
-        @PrimaryKey
+        @PrimaryKey(autoincrement = true)
+        var id : Int = 0,
         var name: String = "",
-
+        var weeks : Weeks = Weeks(),
         var location: String = "",
         var syllabus: String = "",
         var email: String = "",
@@ -17,12 +19,7 @@ class ClassDetails (
         var hours: Int? = null,
         var image: String = "",
         var professor: String = "",
-        var finals: Day = Day()
-) {
-
-    var week: MutableList<Week> = mutableListOf(Week())
-        get() {
-            //week = Select().from(Week::class).where()
-            return mutableListOf(Week())
-        }
-}
+        var finals: Day = Day(),
+        var icon: String = "",
+        var ringmode: Int? = null
+) : BaseModel()
