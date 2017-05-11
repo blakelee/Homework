e43eab31965ea2053b92685f1acb6d82edcd2912
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:32:05 -0700

    removed unnecessary functions

--- app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassPresenterInterface.kt
+++ app/src/main/kotlin/net/blakelee/homework/interfaces/EditClassPresenterInterface.kt
@@ -1,11 +1,6 @@
 package net.blakelee.homework.interfaces
 
-import net.blakelee.homework.models.ClassDetails
-
-
 interface EditClassPresenterInterface {
-    fun onGetClassSuccess(classDetails : ClassDetails)
-    fun onGetClassFailure(classDetails : ClassDetails)
     fun onChangeClassSuccess()
     fun onAddClassSuccess()
     fun onAddClassFailure()
