ec017f56dab1804e284eb2523c9120babbc6df75
Blake Oliveira <blake@blakelee.net>
2017-05-11 16:38:07 -0700

    removed unnecessary code

--- app/src/main/kotlin/net/blakelee/homework/viewholders/MainItemViewHolder.kt
+++ app/src/main/kotlin/net/blakelee/homework/viewholders/MainItemViewHolder.kt
@@ -1,16 +1,14 @@
 package net.blakelee.homework.viewholders
 
-import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import net.blakelee.homework.R
-import net.blakelee.homework.activities.ClassDetailsActivity
 import net.blakelee.homework.models.Classes
 import org.jetbrains.anko.find
 
-class MainItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class MainItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
    var icon: ImageView = view.find(R.id.class_icon)
    val name: TextView = view.find(R.id.class_name)
    val times: TextView = view.find(R.id.class_times)
@@ -18,16 +16,6 @@ class MainItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun init(classes: Classes) {
         icon.setImageResource(R.drawable.science)
         name.text = classes.name
-        var s : String = ""
-        for(s2: String in classes.times)
-            s = s + s2 + "\n"
-
-        s = s.dropLast(1)
-        times.text = s
-
-        itemView.setOnClickListener {
-            //TODO: set bundle to name of class instead of passing dummy data
-            itemView?.context?.startActivity(Intent(itemView.context, ClassDetailsActivity().javaClass))
-        }
+        times.text = classes.weeks.getWeeksAsString()
     }
 }
