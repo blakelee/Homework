eadf7a30292217c4bf78eb82084bff5b63fad65e
Blake Oliveira <blake@blakelee.net>
2017-05-15 22:17:46 -0700

    Added blob functions

--- app/src/main/kotlin/net/blakelee/homework/utils/tools.kt
+++ app/src/main/kotlin/net/blakelee/homework/utils/tools.kt
@@ -8,6 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.AlertBuilder
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.raizlabs.android.dbflow.data.Blob
+import java.io.ByteArrayOutputStream
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
@@ -20,4 +24,14 @@ fun AlertBuilder<DialogInterface>.multiItems(ctx: Context, items: List<CharSeque
     }
     builder.create()
     builder.show()
+}
+
+fun BitmapToBlob(bitmap: Bitmap) : Blob {
+    val baos = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
+    return Blob(baos.toByteArray())
+}
+
+fun BlobToBitmap(blob: Blob) : Bitmap {
+    return BitmapFactory.decodeByteArray(blob.blob, 0, blob.blob.size)
 }
