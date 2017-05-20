6e796ec4da8b0fe7d983ca27aeb11018f5e821dd
Blake Oliveira <blake@blakelee.net>
2017-05-20 14:29:43 -0700

    no need for making my own AtomicInteger impl

--- app/src/main/kotlin/net/blakelee/homework/utils/counter.kt
+++ app/src/main/kotlin/net/blakelee/homework/utils/counter.kt
@@ -1,8 +1,5 @@
 package net.blakelee.homework.utils
 
-object counter {
-    var counter : Int = 0
-    fun increment() = counter++
-    fun decrement() = counter--
-    fun get() = counter
-}
+import java.util.concurrent.atomic.AtomicInteger
+
+var counter = AtomicInteger()
