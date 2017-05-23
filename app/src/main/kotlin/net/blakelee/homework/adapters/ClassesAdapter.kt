bf9f517daba69bf1fd7d7408183a56e777c86d4b
Blake Oliveira <blake@blakelee.net>
2017-05-23 13:39:21 -0700

    Moved listeners to onBind

--- app/src/main/kotlin/net/blakelee/homework/adapters/ClassesAdapter.kt
+++ app/src/main/kotlin/net/blakelee/homework/adapters/ClassesAdapter.kt
@@ -6,49 +6,45 @@ import android.widget.ImageView
 import android.widget.TextView
 import net.blakelee.homework.R
 import net.blakelee.homework.activities.ClassDetailsActivity
+import net.blakelee.homework.activities.MainActivity
 import net.blakelee.homework.base.BaseAdapter
 import net.blakelee.homework.base.BaseViewHolder
-import net.blakelee.homework.interfaces.MainInterface
 import net.blakelee.homework.models.Classes
 import net.blakelee.homework.views.items.MainItemUI
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
-class ClassesAdapter(private val mainInterface: MainInterface) : BaseAdapter<Classes, ClassesAdapter.ClassesViewHolder>() {
+class ClassesAdapter : BaseAdapter<Classes, ClassesAdapter.ClassesViewHolder>() {
 
     override fun getItemViewId(): Int = R.id.classList
 
     override fun instantiateViewHolder(view: View?): ClassesViewHolder = ClassesViewHolder(view)
 
-    override fun onBindViewHolder(holder: ClassesViewHolder, position: Int) {
-        holder.onBind(getItem(position))
-        val item = holder.itemView
-        val id = holder.id
-
-        item.setOnClickListener {
-            item.context.startActivity<ClassDetailsActivity>("class_id" to id)
-        }
-
-        item.setOnLongClickListener {
-            mainInterface.showMenu(holder.name.text.toString(), id)
-        }
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ClassesViewHolder =
         ClassesAdapter.ClassesViewHolder(MainItemUI().createView(AnkoContext.create(parent!!.context, parent)))
 
-    class ClassesViewHolder(view: View?) : BaseViewHolder<Classes>(view) {
+    class ClassesViewHolder(val view: View?) : BaseViewHolder<Classes>(view) {
         val icon: ImageView = view!!.find<ImageView>(R.id.class_icon)
         val name: TextView = view!!.find<TextView>(R.id.class_name)
         val times: TextView = view!!.find<TextView>(R.id.class_times)
         var id: Long = 0
+
+        val activity = itemView.context as MainActivity?
 
         override fun onBind(item: Classes) {
             icon.setImageResource(R.drawable.science)
             name.text = item.name
             times.text = item.weeks.getWeeksAsString()
             id = item.id
+
+            itemView.setOnClickListener {
+                itemView.context.startActivity<ClassDetailsActivity>("class_id" to id)
+            }
+
+            itemView.setOnLongClickListener {
+                activity!!.showMenu(name.text.toString(), id)
+            }
         }
     }
 }
