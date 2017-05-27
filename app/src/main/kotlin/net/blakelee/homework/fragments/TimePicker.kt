6a72e6f47dfd66ab7a2cb26569f439d1b7228b75
Blake Oliveira <blake@blakelee.net>
2017-05-27 14:00:58 -0700

    Fixed timepicker set

--- app/src/main/kotlin/net/blakelee/homework/fragments/TimePicker.kt
+++ app/src/main/kotlin/net/blakelee/homework/fragments/TimePicker.kt
@@ -10,13 +10,13 @@ import net.blakelee.homework.utils.getHour
 import org.jetbrains.anko.ctx
 import java.util.*
 
-class TimePicker (var date: Date, val set: (Date) -> Unit): DialogFragment(), TimePickerDialog.OnTimeSetListener {
+class TimePicker (var date: Date, val position: Int, val set: (Date, Int) -> Unit): DialogFragment(), TimePickerDialog.OnTimeSetListener {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return TimePickerDialog(ctx, this, date.hours, date.minutes, false)
     }
 
     override fun onTimeSet(timeView: TimePicker?, hour: Int, minute: Int) {
-        set(getHour(hour, minute))
+        set(getHour(hour, minute), position)
     }
 }
