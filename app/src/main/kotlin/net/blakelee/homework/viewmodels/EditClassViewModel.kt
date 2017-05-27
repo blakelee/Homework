26d96913b99129082fa397d765355a3ceaaa79e9
Blake Oliveira <blake@blakelee.net>
2017-05-27 14:00:23 -0700

    Added add/remove/setters to vm

--- app/src/main/kotlin/net/blakelee/homework/viewmodels/EditClassViewModel.kt
+++ app/src/main/kotlin/net/blakelee/homework/viewmodels/EditClassViewModel.kt
@@ -2,6 +2,8 @@ package net.blakelee.homework.viewmodels
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MediatorLiveData
+import android.arch.lifecycle.MutableLiveData
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
 import net.blakelee.homework.App
@@ -12,29 +14,43 @@ import net.blakelee.homework.utils.ClassValidation
 import net.blakelee.homework.utils.counter
 import org.jetbrains.anko.AnkoLogger
 import java.io.File
+import java.util.*
 
 class EditClassViewModel(application: Application) : AndroidViewModel(application), AnkoLogger {
-    private var db : AppDatabase = AppDatabase.createPersistentDatabase(application)
+    private var db: AppDatabase = AppDatabase.createPersistentDatabase(application)
+    var classDetails: MutableLiveData<ClassDetails> = MutableLiveData()
+    var weeks = MediatorLiveData<MutableList<Week>>()
 
-    fun insertClass(cd : ClassDetails) {
-        val id = db.classModel().insertClass(cd)
+    fun insertClass() {
+        val id = db.classModel().insertClass(classDetails.value!!)
         setImage(id.toString())
     }
-    fun updateClass(cd : ClassDetails) {
-        db.classModel().updateClass(cd)
-        setImage(cd.id.toString())
+
+    fun updateClass() {
+        db.classModel().updateClass(classDetails.value!!)
+        setImage(classDetails.value!!.id.toString())
     }
-    fun getClass(id : Long) = db.classModel().getClassById(id)
 
-    fun validate(cd : ClassDetails) : ClassValidation {
+    fun loadClassById(id: Long?) {
+        if (id == null)
+            classDetails.value = ClassDetails()
+        else
+            classDetails.value = db.classModel().getClassById(id)
 
-        if (cd.name.isEmpty())
+        weeks.addSource(classDetails) {
+            it?.let { weeks.value = it.weeks.week }
+        }
+    }
+
+    fun validate() : ClassValidation {
+
+        if (classDetails.value!!.name.isEmpty())
             return ClassValidation.EMPTY
 
-        if (db.classModel().findByName(cd.name) != null)
+        if (db.classModel().findByName(classDetails.value!!.name) != null)
             return ClassValidation.CONFLICT
 
-        for(item : Week in cd.weeks.week) {
+        for(item : Week in classDetails.value!!.weeks.week) {
             val week = item.day
 
             if (week.isEmpty())
@@ -61,5 +77,20 @@ class EditClassViewModel(application: Application) : AndroidViewModel(applicatio
                 temp.renameTo(file)
             }
         }
+    }
+
+    fun addWeek() = weeks.value!!.add(weeks.value!!.size, Week())
+    fun removeWeek(position: Int) = weeks.value!!.removeAt(position)
+
+    fun setDay(daysSelected: List<Int>, position: Int) {
+        weeks.value!![position].day = daysSelected
+    }
+
+    fun setStartTime(date: Date, position: Int) {
+        classDetails.value!!.weeks.week[position].startTime = date
+    }
+
+    fun setEndTime(date: Date, position: Int) {
+        classDetails.value!!.weeks.week[position].endTime = date
     }
 }
