bfadeb523c0cb38f67feed61647d6b7ac6f950c6
Blake Oliveira <blake@blakelee.net>
2017-05-29 23:08:17 -0700

    use lovelychoicedialog

--- app/src/main/kotlin/net/blakelee/homework/fragments/DayPicker.kt
+++ app/src/main/kotlin/net/blakelee/homework/fragments/DayPicker.kt
@@ -1,37 +1,31 @@
 package net.blakelee.homework.fragments
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
 import android.os.Bundle
 import android.text.format.DateUtils.LENGTH_LONG
 import android.text.format.DateUtils.getDayOfWeekString
+import com.yarolegovich.lovelydialog.LovelyChoiceDialog
+import net.blakelee.homework.R
 import net.blakelee.homework.interfaces.EditClassInterface
-import java.util.*
+import org.jetbrains.anko.ctx
 
 class DayPicker(val oldDaysSelected : List<Int>, val position : Int) : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(activity)
         val ba = BooleanArray(7)
-        val daysSelected : ArrayList<Int> = ArrayList(oldDaysSelected)
 
         oldDaysSelected.map { ba[it] = true }
 
-        with(builder) {
-            setTitle("Class Days")
-            setMultiChoiceItems(Array(7) { i -> getDayOfWeekString(i + 1, LENGTH_LONG) }, ba) { _, which, isChecked ->
-                if (isChecked)
-                    daysSelected.add(which)
-                else
-                    daysSelected.remove(which)
-            }
-            setPositiveButton("OK") { _, _ ->
-                daysSelected.sort()
-                val listener = activity as EditClassInterface
-                listener.onFinishEditDialog(daysSelected.toList(), position)
-            }
+        return LovelyChoiceDialog(ctx)
+                .setTitle("Class Days")
+                .setIcon(R.drawable.ic_calendar_7)
+                .setTopColorRes(R.color.accent)
+                .setItemsMultiChoice(Array(7) { i -> getDayOfWeekString(i + 1, LENGTH_LONG) }, ba) { position, _ ->
+                    val listener = activity as EditClassInterface
+                    listener.onFinishEditDialog(position, this.position)
+                }
+                .setConfirmButtonText("OK")
+                .create()
         }
-        return builder.create()
-    }
 }
