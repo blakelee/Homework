11e7d506d80f70f1055e04cf544dbdf88e393652
Blake Oliveira <blake@blakelee.net>
2017-05-29 23:11:48 -0700

    delete leftover images

--- app/src/main/kotlin/net/blakelee/homework/viewmodels/MainActivityViewModel.kt
+++ app/src/main/kotlin/net/blakelee/homework/viewmodels/MainActivityViewModel.kt
@@ -3,8 +3,12 @@ package net.blakelee.homework.viewmodels
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import net.blakelee.homework.App
 import net.blakelee.homework.databases.AppDatabase
 import net.blakelee.homework.models.Classes
+import java.io.File
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
     private var db : AppDatabase = AppDatabase.createPersistentDatabase(application)
@@ -14,6 +18,10 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     fun deleteClass(id: Long) {
         db.classModel().deleteClass(id)
 
-        //TODO: Need to delete images here
+        async(CommonPool) {
+            val context = getApplication<App>().applicationContext
+            val file = File(context.filesDir, id.toString() + "-image")
+            file.delete()
+        }
     }
 }
