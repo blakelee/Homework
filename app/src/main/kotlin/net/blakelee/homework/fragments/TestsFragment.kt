e0d1ef9baf38c3afa3c2669e52ea9e33df31872c
Blake Oliveira <blake@blakelee.net>
2017-06-04 18:29:33 -0700

    initial stubs

---
+++ app/src/main/kotlin/net/blakelee/homework/fragments/TestsFragment.kt
@@ -0,0 +1,14 @@
+package net.blakelee.homework.fragments
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TestsFragment(classId: Long): Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
