7134f74570491959426257c6d229acf880f8f0bf
Blake Oliveira <blake@blakelee.net>
2017-06-09 00:36:50 -0700

    kotlin update fixed having to rename parameters for kapt

--- app/src/main/kotlin/net/blakelee/homework/databases/ClassDao.kt
+++ app/src/main/kotlin/net/blakelee/homework/databases/ClassDao.kt
@@ -4,28 +4,28 @@ import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.*
 import net.blakelee.homework.models.ClassDetails
 import net.blakelee.homework.models.Classes
-import net.blakelee.homework.utils.WeeksConverters
+import net.blakelee.homework.utils.WeekConverter
 
 @Dao
-@TypeConverters(WeeksConverters::class)
+@TypeConverters(WeekConverter::class)
 interface ClassDao {
 
     @Insert
     fun insertClass(cd : ClassDetails) : Long
 
-    @Query("SELECT name FROM class_details WHERE name = :p0 LIMIT 1")
+    @Query("SELECT name FROM class_details WHERE name = :name LIMIT 1")
     fun findByName(name : String) : String?
 
-    @Query("SELECT name FROM class_details WHERE name = :p0 AND id != :p1 LIMIT 1")
+    @Query("SELECT name FROM class_details WHERE name = :name AND id != :id LIMIT 1")
     fun findByNameGivenId(name: String, id: Long?): String?
 
-    @Query("SELECT * FROM class_details WHERE id = :p0 LIMIT 1")
+    @Query("SELECT * FROM class_details WHERE id = :id LIMIT 1")
     fun getClassById(id : Long) : ClassDetails
 
     @Query("SELECT id, name, weeks, icon, icon_color FROM class_details")
     fun getClasses() : LiveData<List<Classes>>
 
-    @Query("DELETE FROM class_details WHERE id = :p0")
+    @Query("DELETE FROM class_details WHERE id = :id")
     fun deleteClass(id : Long)
 
     @Update
