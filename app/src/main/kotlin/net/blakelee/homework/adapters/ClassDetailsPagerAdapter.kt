3a4b72d2b2cc2d501da293ffd0220fd373b64073
Blake Oliveira <blake@blakelee.net>
2017-06-09 00:38:12 -0700

    add color

--- app/src/main/kotlin/net/blakelee/homework/adapters/ClassDetailsPagerAdapter.kt
+++ app/src/main/kotlin/net/blakelee/homework/adapters/ClassDetailsPagerAdapter.kt
@@ -4,15 +4,20 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import net.blakelee.homework.fragments.ClassDetailsMainFragment
-import net.blakelee.homework.models.ClassDetails
+import net.blakelee.homework.fragments.HomeworkFragment
+import net.blakelee.homework.fragments.NotesFragment
+import net.blakelee.homework.fragments.TestsFragment
 
-class ClassDetailsPagerAdapter(fragmentManager: FragmentManager, var numTabs: Int, val classId: Long) : FragmentStatePagerAdapter(fragmentManager){
+class ClassDetailsPagerAdapter(fragmentManager: FragmentManager, var numTabs: Int, val classId: Long, val color: Int) : FragmentStatePagerAdapter(fragmentManager){
 
     override fun getCount(): Int = numTabs
 
     override fun getItem(position: Int): Fragment? {
         when (position) {
             0 -> return ClassDetailsMainFragment(classId)
+            1 -> return HomeworkFragment(classId, color)
+            2 -> return TestsFragment(classId, color)
+            3 -> return NotesFragment(classId, color)
             else -> return null
         }
     }
