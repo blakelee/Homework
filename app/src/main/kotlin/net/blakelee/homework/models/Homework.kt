0243623203b051937eded9522240a8af4947201a
Blake Oliveira <blake@blakelee.net>
2017-06-09 00:41:18 -0700

    add more details

--- app/src/main/kotlin/net/blakelee/homework/models/Homework.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/Homework.kt
@@ -1,5 +1,13 @@
 package net.blakelee.homework.models
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "homework")
 class Homework: BaseAssignment() {
+
+    @PrimaryKey(autoGenerate = true)
+    var hw_id: Long? = null
+
     var problems: String = ""
 }
