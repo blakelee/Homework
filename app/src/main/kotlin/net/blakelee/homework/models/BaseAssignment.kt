edc76da4659d7bec282f52c60afb6c060a79d8ad
Blake Oliveira <blake@blakelee.net>
2017-06-09 00:35:36 -0700

    init menus

--- app/src/main/kotlin/net/blakelee/homework/models/BaseAssignment.kt
+++ app/src/main/kotlin/net/blakelee/homework/models/BaseAssignment.kt
@@ -1,11 +1,30 @@
 package net.blakelee.homework.models
 
+import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.TypeConverters
+import net.blakelee.homework.utils.DateConverter
+import net.blakelee.homework.utils.ReminderConverter
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 open class BaseAssignment {
     var id: Long = 0
     var title: String = ""
     var description: String = ""
+
+    @TypeConverters(DateConverter::class)
     var due: Date? = null
+
+    @TypeConverters(ReminderConverter::class)
     var reminders: MutableList<Reminder>? = null
+
+    @Ignore
+    var time : DateFormat = SimpleDateFormat("h:mma", Locale.getDefault())
+
+    @Ignore
+    var day: DateFormat = SimpleDateFormat("EE", Locale.getDefault())
+
+    fun getTime(): String = time.format(due)
+    fun getDay(): String = day.format(due)
 }
